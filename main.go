30a9fd292cd6567db41b94e0d9489ada6aa9c814
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-02-23 12:14:11 +0300

    added the types Movie and Director to the main function and the called the HandleFunc method

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName string `json:"lastname"`
+}
+
+var movies []Movie
+
 func main(){
-	
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe("8000",r))
 }
